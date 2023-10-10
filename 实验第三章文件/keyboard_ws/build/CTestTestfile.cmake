feea7dd3ac7ade7f56b85949b1fa0ca3ac8eebaa
zzh <864605475@qq.com>
2023-10-10 10:03:35 +0800

    stumansys

---
+++ 实验第三章文件/keyboard_ws/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/zzh/keyboard_ws/src
+# Build directory: /home/zzh/keyboard_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("gtest")
+subdirs("teleop_twist_keyboard")
---
+++ 实验第三章文件/keyboard_ws/build/catkin_generated/order_packages.py
@@ -0,0 +1,5 @@
+# generated from catkin/cmake/template/order_packages.context.py.in
+source_root_dir = '/home/zzh/keyboard_ws/src'
+whitelisted_packages = ''.split(';') if '' != '' else []
+blacklisted_packages = ''.split(';') if '' != '' else []
+underlay_workspaces = '/home/zzh/turtlebot_ws/devel;/opt/ros/melodic'.split(';') if '/home/zzh/turtlebot_ws/devel;/opt/ros/melodic' != '' else []
